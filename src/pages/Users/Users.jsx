30c181da553316ea3845f6b10e0925fea177bf6f
Mihai Palienko <mihai807@mail.ru>
2025-09-30 20:33:02 +0300

    feat: users

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -6,6 +6,8 @@ import User from "../../components/User/User";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { API_URL } from "../../utiles/api";
+import { HEADERS } from "./consts";
 
 export default function Users() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -13,7 +15,7 @@ export default function Users() {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/users");
+      const response = await axios.get(`${API_URL}/users`);
       setUsers(response.data);
     } catch (err) {
       console.error("Ошибка при загрузке пользователей:", err);
@@ -26,7 +28,7 @@ export default function Users() {
 
   const handleDeleteUser = async (userId) => {
     try {
-      await axios.delete(`http://localhost:5000/users/${userId}`);
+      await axios.delete(`${API_URL}/users/${userId}`);
       alert("Пользователь удален");
       fetchUsers();
     } catch (error) {
@@ -44,74 +46,63 @@ export default function Users() {
     setIsModalOpen(false);
   };
 
+  const onCreateUser = async (formValues) => {
+    const response = await axios.post(`${API_URL}/users`, formValues);
+    fetchUsers();
+  };
+
+  
+
   return (
     <div>
-      <div className="wrapper"></div>
+      <div className='wrapper'></div>
       <Header />
-      <section className="users container">
-        <div className="users__title">
-          <h3 className="title">Users</h3>
-          <div className="users__button">
-            <button onClick={openModal} className="button">
+      <section className='users container'>
+        <div className='users__title'>
+          <h3 className='title'>Users</h3>
+          <div className='users__button'>
+            <button onClick={openModal} className='button'>
               Add User
             </button>
-            {isModalOpen && <Modal onClose={closeModal} />}
-            <img className="button__img" src="./plus.svg" alt="+" />
+            {isModalOpen && <Modal onCreateUser={onCreateUser} onClose={closeModal} />}
+            <img className='button__img' src='./plus.svg' alt='+' />
           </div>
         </div>
-        <nav className="users__link">
-          <a href="" className="link__active">
+        <nav className='users__link'>
+          <a href='' className='link__active'>
             Administration
           </a>
-          <a href="" className="link">
+          <a href='' className='link'>
             General Partners
           </a>
-          <a href="" className="link">
+          <a href='' className='link'>
             Wealt Managers
           </a>
         </nav>
-        <div className="users__container">
-          <div className="users__top">
-            <div className="users__input">
-              <img src="./search.svg" alt="" className="input__img" />
-              <input
-                className="input"
-                type="text"
-                name=""
-                id=""
-                placeholder="Searh Users"
-              />
+        <div className='users__container'>
+          <div className='users__top'>
+            <div className='users__input'>
+              <img src='./search.svg' alt='' className='input__img' />
+              <input className='input' type='text' name='' id='' placeholder='Searh Users' />
             </div>
-            <div className="users__left">
-              <img src="./filter.svg" alt="" className="users__left__img" />
-              <button className="users__left__button">Filters</button>
+            <div className='users__left'>
+              <img src='./filter.svg' alt='' className='users__left__img' />
+              <button className='users__left__button'>Filters</button>
             </div>
           </div>
-          <div className="users__table">
-            <div className="table">
-              <div className="table__info">
-                <div className="table__header">
-                  <div className="header__name">
-                    <div className="table__checkbox"></div>
-                    <p className="name">Name</p>
-                    <img src="./chevron1.svg" alt="" className="table__img" />
-                  </div>
-                  <div className="header__name">
-                    <p className="name">Email</p>
-                    <img src="./chevron1.svg" alt="" className="table__img" />
-                  </div>
-                  <div className="header__name">
-                    <p className="name">Role</p>
-                    <img src="./chevron1.svg" alt="" className="table__img" />
-                  </div>
-                  <div className="header__name">
-                    <p className="name">Created on</p>
-                    <img src="./chevron1.svg" alt="" className="table__img" />
-                  </div>
-                  <div className="header__name">
-                    <p className="name">Status</p>
-                    <img src="./chevron1.svg" alt="" className="table__img" />
-                  </div>
+          <div className='users__table'>
+            <div className='table'>
+              <div className='table__info'>
+                <div className='table__header'>
+                  {HEADERS.map((header) => {
+                    return (
+                      <div className='header__name'>
+                        <div className='table__checkbox'></div>
+                        <p className='name'>{header}</p>
+                        <img src='./chevron1.svg' alt='' className='table__img' />
+                      </div>
+                    );
+                  })}
                 </div>
 
                 <div>
